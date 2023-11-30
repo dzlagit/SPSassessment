3e960cacf4b95bbbf641544692f4a31776fab5c5
dzlagit <146720843+dzlagit@users.noreply.github.com>
2023-11-30 17:16:15 +0000

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,127 @@
+greetings_dict = {
+  "english": [
+      "Hello",
+      "Hi",
+      "Hey",
+      "Good morning",
+      "Good afternoon",
+      "Good evening",
+      "Howdy",
+      "Greetings",
+      "Salutations",
+      "What's up",
+  ],
+  "spanish": [
+      "Hola",
+      "Buenos días",
+      "Buenas tardes",
+      "Buenas noches",
+      "¿Qué tal?",
+      "Saludos",
+  ],
+  "french": [
+      "Bonjour",
+      "Salut",
+      "Bonsoir",
+      "Comment ça va?",
+      "Bienvenue",
+  ],
+  "german": [
+      "Hallo",
+      "Guten Morgen",
+      "Guten Tag",
+      "Guten Abend",
+      "Wie geht's",
+      "Willkommen",
+  ],
+  "italian": [
+      "Ciao",
+      "Buongiorno",
+      "Buon pomeriggio",
+      "Buona sera",
+      "Come stai",
+      "Benvenuto",
+  ],
+  "japanese": [
+      "こんにちは (Konnichiwa)",
+      "おはようございます (Ohayou gozaimasu)",
+      "こんばんは (Konbanwa)",
+      "元気ですか？ (Genki desu ka?)",
+  ],
+  "chinese": [
+      "你好 (Nǐ hǎo)",
+      "早上好 (Zǎoshang hǎo)",
+      "下午好 (Xiàwǔ hǎo)",
+      "晚上好 (Wǎnshàng hǎo)",
+  ],
+  "russian": [
+      "Привет (Privet)",
+      "Доброе утро (Dobroe utro)",
+      "Добрый день (Dobryy den')",
+      "Добрый вечер (Dobryy vecher)",
+      "Как дела? (Kak dela?)",
+  ],
+  "arabic": [
+      "السلام عليكم (As-salamu alaykum)",
+      "صباح الخير (Sabah al-khayr)",
+      "مساء الخير (Masa' al-khayr)",
+      "كيف حالك؟ (Kayfa halak?)",
+  ],
+}
+
+jokes_dict = {
+    "joke1": "Why don't scientists trust atoms? Because they make up everything!",
+    "joke2": "Parallel lines have so much in common. It's a shame they'll never meet!",
+    "joke3": "I told my wife she should embrace her mistakes! She gave me a hug!",
+    "joke4": "What do you get when you cross a snowman and a vampire? Frostbite!",
+    "joke5": "I used to play piano by ear, but now I use my hands and fingers!",
+    "joke6": "Why did the scarecrow win an award? He was outstanding in his field!",
+    "joke7": "I'm reading a book on anti-gravity! It's impossible to put down!",
+    "joke8": "Why don't oysters donate to charity? Because they are shellfish!",
+    "joke9": "I only know 25 letters of the alphabet! I don't know y!",
+    "joke10": "What did one hat say to the other? Stay here, I'm going on ahead!",
+    "joke11": "Why did the bicycle fall over? Because it was two-tired!",
+    "joke12": "What's orange and sounds like a parrot? A carrot!",
+    "joke13": "Why don't skeletons fight each other? They don't have the guts!",
+    "joke14": "Why did the tomato turn red? Because it saw the salad dressing!",
+}
+
+inputJoke_dict = {
+  "tell me a joke": None
+}
+
+howAreYou_dict = {
+    "response1": "I'm doing well, thank you!",
+    "response2": "Not too bad, how about you?",
+    "response3": "I'm great! How can I assist you today?",
+    "response4": "I'm feeling fantastic!",
+    "response5": "Could be better, but I'm hanging in there.",
+    "response6": "I'm doing fine, thanks for asking!",
+    "response7": "Pretty good! Anything exciting happening?",
+    "response8": "Not the best day, but I'll manage.",
+    "response9": "I'm so-so, thanks for checking in.",
+    "response10": "I'm feeling wonderful!",
+}
+
+inputHowAreYou_dict = {
+  "how are you": None
+}
+
+created_dict = {
+    "who created you": None,
+    "who are you": None,
+    "who made you": None,
+    "who is your creator": None,
+}
+
+
+validation_dict = {
+  "arthur": None
+}
+
+name_dict = {
+  "what is your name": "My name is Arthobot!",
+  "what are you called": "I am called Arthobot",
+  "what can i call you": "I am called Arthobot",
+  "how can i address you": "You can call me Arthobot!",
+}
