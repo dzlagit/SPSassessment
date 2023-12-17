6b9cbf46286cc3c58639284af5e39d4721f51814
dzlagit <146720843+dzlagit@users.noreply.github.com>
2023-12-17 23:50:08 +0000

    Update 1 main.py

--- main.py
+++ main.py
@@ -2,45 +2,48 @@ import random
 from dict import (
     greetings_dict, jokes_dict, inputJoke_dict,
     howAreYou_dict, inputHowAreYou_dict,
-    created_dict, validation_dict, name_dict  #importing the dictionaries from dict.py
+    created_dict, validation_dict, name_dict  # importing the dictionaries from dict.py
 )
 
-def get_random_greeting(user_language): #function to get random greeting
-    language_greetings = greetings_dict.get(user_language.lower()) #get the language from the dictionary
+def get_random_greeting(user_language):
+    language_greetings = greetings_dict.get(user_language.lower())
 
-    if language_greetings: #if the language exists
-        return random.choice(language_greetings) #return a random greeting
+    if language_greetings:
+        return random.choice(language_greetings)
     else:
-        return "I'm sorry, I don't have greetings in that language." #if the language doesn't exist, return this
+        raise ValueError("Language not found in the greetings dictionary.")
 
-while True: #loop for the language
-    user_language = input("Enter the language for your greeting: ") #ask for the language
+try:
+    while True:
+        try:
+            user_language = input("Enter the language for your greeting: ")
+            greeting = get_random_greeting(user_language)
+            print(greeting)
+            while True:
+                user_input = input(">>> ").lower()
+                if user_input in inputJoke_dict:
+                    print(random.choice(list(jokes_dict.values())))
+                elif user_input in created_dict:
+                    validationInput = input("What is your name: ").lower()
+                    if validationInput in validation_dict:
+                        print("You created me!")
+                    else:
+                        print("My creator is Arthur!")
+                elif user_input in inputHowAreYou_dict:
+                    print(random.choice(list(howAreYou_dict.values())))
+                elif user_input in name_dict:
+                    print(name_dict[user_input])
+                elif user_input == "goodbye":
+                    print("Goodbye! Have a great day.")
+                    break
+                else:
+                    print("I'm sorry, I don't understand that command.")
 
-    if user_language.lower() in greetings_dict: #check if the specified language exists
-
-        greeting = get_random_greeting(user_language)  #get a random greeting
-        print(greeting) #print the greeting
-
-       
-        while True:  #loop that ends with "goodbye"
-            user_input = input(">>> ").lower() #ask for the input and always make it lowercase
-            if user_input in inputJoke_dict: #if the input is in the joke dictionary
-                print(random.choice(list(jokes_dict.values()))) #print a random joke
-            elif user_input in created_dict:  #if the input is in the created dictionary
-                validationInput = input("What is your name: ").lower() #ask for the name and make it lowercase
-                if validationInput in validation_dict: #if the name is in the validation dictionary
-                    print("You created me!") 
-                else: #if the name is not in the validation dictionary
-                    print("My creator is Arthur!") 
-            elif user_input in inputHowAreYou_dict: #if the input is in the how are you dictionary
-                print(random.choice(list(howAreYou_dict.values()))) #print a random response
-            elif user_input in name_dict: #if the input is in the name dictionary
-                print(name_dict[user_input]) #print the response
-            elif user_input == "goodbye": #if the input is goodbye
-                print("Goodbye! Have a great day.") 
-                break #break out of the inner loop
-            else: #if the input is not in any of the dictionaries 
-                print("I'm sorry, I don't understand that command.") 
-        break  #break out of the outer loop when the language is found
-    else:
-        print("Language not found in the greetings dictionary. Please try again.") #if the language is not found, print this
+        except ValueError as ve:
+            print(f"Error: {ve}")
+        except KeyError:
+            print("Unexpected input. Please try again.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+except KeyboardInterrupt:
+    print("\nExiting the program. Goodbye!")
