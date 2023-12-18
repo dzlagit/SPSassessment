eaee83702c13b070586c1686f4b11bb56faab441
dzlagit <146720843+dzlagit@users.noreply.github.com>
2023-12-18 00:16:58 +0000

    Update3 main.py
    final comments

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-import random
-from dict import (
+import random  # importing the random module
+from dict import ( # importing the dictionaries from dict.py
     greetings_dict, jokes_dict, inputJoke_dict,
     howAreYou_dict, inputHowAreYou_dict,
     created_dict, validation_dict, name_dict  # importing the dictionaries from dict.py
 )
 
-def get_random_greeting(user_language):
-    language_greetings = greetings_dict.get(user_language.lower())
+def get_random_greeting(user_language): # function to get a random greeting
+    language_greetings = greetings_dict.get(user_language.lower()) # getting the language from the dictionary
 
-    if language_greetings:
-        return random.choice(language_greetings)
+    if language_greetings: # if the language is found in the dictionary
+        return random.choice(language_greetings) # return a random greeting from the dictionary
     else:
-        raise ValueError("Language not found in the greetings dictionary.")
+        raise ValueError("Language not found in the greetings dictionary.") # else raise an error
 
 try: # try and except block to catch errors 
-    while True:
+    while True: # while loop to keep the program running
         try: 
-            user_language = input("Enter the language for your greeting: ")
-            greeting = get_random_greeting(user_language)
+            user_language = input("Enter the language for your greeting: ") # getting the language from the user
+            greeting = get_random_greeting(user_language) # calling the function to get a random greeting
             print(greeting)
-            while True:
-                user_input = input(">>> ").lower()
-                if user_input in inputJoke_dict:
-                    print(random.choice(list(jokes_dict.values())))
-                elif user_input in created_dict:
-                    validationInput = input("What is your name: ").lower()
-                    if validationInput in validation_dict:
+            while True: # while loop to keep the program running
+                user_input = input(">>> ").lower() # getting the input from the user
+                if user_input in inputJoke_dict: # checking if the input is in the dictionary
+                    print(random.choice(list(jokes_dict.values()))) # printing a random joke from the dictionary
+                elif user_input in created_dict: # checking if the input is in the dictionary
+                    validationInput = input("What is your name: ").lower() # getting the input from the user
+                    if validationInput in validation_dict: # checking if the input is in the dictionary
                         print("You created me!")
                     else:
                         print("My creator is Arthur!")
-                elif user_input in inputHowAreYou_dict:
-                    print(random.choice(list(howAreYou_dict.values())))
+                elif user_input in inputHowAreYou_dict: # checking if the input is in the dictionary
+                    print(random.choice(list(howAreYou_dict.values()))) # printing a random response from the dictionary
                 elif user_input in name_dict:
-                    print(name_dict[user_input])
-                elif user_input == "goodbye":
+                    print(name_dict[user_input]) # printing the name from the dictionary
+                elif user_input == "goodbye": # checking if the input is in the dictionary
                     print("Goodbye! Have a great day.")
-                    break
-                else:
+                    break # breaking out of the loop
+                else: # if the input is not in the dictionary
                     print("I'm sorry, I don't understand that command.")
 
         except ValueError as ve: # catching the error if the value is not found in the dictionary
